2b9a573716fc7f4093355e1d3639d30c7c65904a
zono0013 <zono@zonos-Mac.local>
2024-11-06 15:59:04 +0900

    デプロイように編集３

--- client2/mymuse/next.config.ts
+++ client2/mymuse/next.config.ts
@@ -1,12 +1,28 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
     images: {
-        domains: ['lh3.googleusercontent.com'], // ここに外部ホスト名を追加
+        domains: ['lh3.googleusercontent.com'], // 外部画像ホスト名を追加
     },
     eslint: {
         ignoreDuringBuilds: true, // ビルド時のESLintチェックを無効化
+    },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Credentials',
+                        value: 'true',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: process.env.NEXT_PUBLIC_API_BASE_URL || '', // バックエンドのURL（環境変数）を設定
+                    },
+                ],
+            },
+        ];
     },
 };
 
